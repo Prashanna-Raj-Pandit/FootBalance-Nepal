aeb13d1bbdf17207e35ee0af2464f7b3ead9c343
Prashanna Raj Pandit <erprp99@gmail.com>
2024-07-14 16:05:08 +0545

    changed youtube video

--- static/assets/js/youtube.js
+++ static/assets/js/youtube.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
         var iframe = document.createElement('iframe');
         iframe.setAttribute('width', '100%');
         iframe.setAttribute('height', '100%');
-        iframe.setAttribute('src', 'https://www.youtube.com/embed/YFHUTo3Zxk8?autoplay=1&rel=0');
+        iframe.setAttribute('src', 'https://www.youtube.com/embed/gE26SQOiskU?si=NeCtXPCKhgkSo-D6');
         iframe.setAttribute('title', 'YouTube video player');
         iframe.setAttribute('frameborder', '0');
         iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
